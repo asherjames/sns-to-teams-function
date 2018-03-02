e015b47a42c69dc311da3ec3d609e86d17a3115f
asherjames <asherjames.nz@gmail.com>
2018-03-02 18:42:22 +1300

    Add AWS events library, create SnsAlarmMessage data class

--- src/main/kotlin/ash/lambda/SnsHandler.kt
+++ src/main/kotlin/ash/lambda/SnsHandler.kt
@@ -2,19 +2,44 @@ package ash.lambda
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.SNSEvent
+import org.joda.time.DateTime
 
-class SnsHandler : RequestHandler<SnsEvent, Response>
+class SnsHandler : RequestHandler<SNSEvent, Response>
 {
-  override fun handleRequest(input: SnsEvent?, context: Context?): Response
+  override fun handleRequest(input: SNSEvent?, context: Context?): Response
   {
-    return Response()
+    if (input == null)
+    {
+      return Response("Input is null")
+    }
+
+    val alarmMessages = input.records
+        .map { snsRecord -> snsRecord.sns }
+        .map { sns -> sns.message }
+
+
+
+    return Response("Request successfully processed")
   }
 }
 
-data class SnsEvent(val records: List<SnsRecord>)
-
-data class SnsRecord(val EventVersion: String,
-                     val eventSubscriptionArn: String,
-                     val eventSource: String)
-
 data class Response(val message: String = "")
+
+data class SnsAlarmMessage(val alarmName: String,
+                           val alarmDescription: String,
+                           val awsAccountId: String,
+                           val newStateValue: String,
+                           val newStateReason: String,
+                           val stateChangeTime: DateTime,
+                           val region: String,
+                           val oldStateValue: String,
+                           val metricName: String,
+                           val namespace: String,
+                           val statistic: String,
+                           val unit: String,
+                           val dimensions: Pair<String, String>,
+                           val period: Int,
+                           val evaluationPeriods: Int,
+                           val comparisonOperator: String,
+                           val threshold: Double)
