9994feca6d63c41257da8e953c12ab62a1185c94
asherjames <asherjames.nz@gmail.com>
2018-03-03 00:20:43 +1300

    Create remaining data classes, change some libraries

--- src/main/kotlin/ash/lambda/SnsHandler.kt
+++ src/main/kotlin/ash/lambda/SnsHandler.kt
@@ -3,10 +3,23 @@ package ash.lambda
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.SNSEvent
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.apache.commons.text.StringEscapeUtils
 import org.joda.time.DateTime
 
 class SnsHandler : RequestHandler<SNSEvent, Response>
 {
+  private val objectMapper = ObjectMapper()
+      .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+      .registerModule(JodaModule())
+      .registerKotlinModule()
+
   override fun handleRequest(input: SNSEvent?, context: Context?): Response
   {
     if (input == null)
@@ -15,8 +28,9 @@ class SnsHandler : RequestHandler<SNSEvent, Response>
     }
 
     val alarmMessages = input.records
-        .map { snsRecord -> snsRecord.sns }
-        .map { sns -> sns.message }
+        .map { snsRecord -> snsRecord.sns.message }
+        .map { StringEscapeUtils.escapeJson(it) }
+        .map { objectMapper.readValue<SnsAlarmMessage>(it) }
 
 
 
@@ -26,6 +40,8 @@ class SnsHandler : RequestHandler<SNSEvent, Response>
 
 data class Response(val message: String = "")
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class SnsAlarmMessage(val alarmName: String,
                            val alarmDescription: String,
                            val awsAccountId: String,
@@ -34,12 +50,21 @@ data class SnsAlarmMessage(val alarmName: String,
                            val stateChangeTime: DateTime,
                            val region: String,
                            val oldStateValue: String,
-                           val metricName: String,
-                           val namespace: String,
-                           val statistic: String,
-                           val unit: String,
-                           val dimensions: Pair<String, String>,
-                           val period: Int,
-                           val evaluationPeriods: Int,
-                           val comparisonOperator: String,
-                           val threshold: Double)
+                           val trigger: Trigger)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Trigger(val metricName: String,
+                   val namespace: String,
+                   val statistic: String,
+                   val unit: String?,
+                   val dimensions: List<Dimension>,
+                   val period: Int,
+                   val evaluationPeriods: Int,
+                   val comparisonOperator: String,
+                   val threshold: Double)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Dimension(val name: String,
+                     val value: String)
